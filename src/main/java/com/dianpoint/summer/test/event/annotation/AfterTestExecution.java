e5731c0631c01542307e6a6385818c6772524945
MorningBells <85073439@qq.com>
2023-04-06 21:52:11 +0800

    1. 搭建event测试结构，增加注解

---
+++ src/main/java/com/dianpoint/summer/test/event/annotation/AfterTestExecution.java
@@ -0,0 +1,22 @@
+package com.dianpoint.summer.test.event.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * @author wangyi
+ * @date 2023/4/6
+ */
+@Retention(RUNTIME)
+@Target({ METHOD, ANNOTATION_TYPE })
+@Documented
+public @interface AfterTestExecution {
+
+    String value() default "";
+
+}
---
+++ src/main/java/com/dianpoint/summer/test/event/annotation/AfterTestMethod.java
@@ -0,0 +1,19 @@
+package com.dianpoint.summer.test.event.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * @author wangyi
+ * @date 2023/4/6
+ */
+@Retention(RUNTIME)
+@Target({ METHOD, ANNOTATION_TYPE })
+@Documented
+public @interface AfterTestMethod {
+}
